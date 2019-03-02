65595606ced88d458525f581e7468ac586df7c1d
Matthew Harrilal <matthewharrilal@gmail.com>
2019-03-02 12:16:05 -0800

    Added channel declaration ... going to add queue declaration and proceed to pass events through

--- main.go
+++ main.go
@@ -12,3 +12,17 @@ func FailOnError(err error, msg string) {
 		log.Fatalf("%v:  %v", msg, err)
 	}
 }
+
+func main() {
+	connection, err := amqp.Dial("amqp://guest:guest@localhost:5672/")
+	FailOnError(err, "Could not connect to Rabbit MQ services!")
+
+	// Close connection after all subsequent code in main has been executed 
+	defer connection.Close()
+
+	channel, err := connection.Channel()
+	FailOnError(err, "Failed to create channel")
+
+	// Close channel after main function has been executed
+	defer channel.Close()
+}
