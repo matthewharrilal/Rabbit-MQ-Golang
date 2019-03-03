5e4a693505c89ead0386207ac6bdb8e85bc44f4e
Matthew Harrilal <matthewharrilal@gmail.com>
2019-03-03 11:17:17 -0800

    Documented the use of the forever channel and refactored out the declaration of the queue twice

--- recieve.go
+++ recieve.go
@@ -21,24 +21,14 @@ func main() {
 	failOnError(err, "Failed to open a channel")
 	defer ch.Close()
 
-	q, err := ch.QueueDeclare(
-		"testQueue", // name
-		true,       // durable
-		false,       // delete when unused
-		false,       // exclusive
-		false,       // no-wait
-		nil,         // arguments
-	)
-	failOnError(err, "Failed to declare a queue")
-
 	msgs, err := ch.Consume(
-		q.Name, // queue
-		"",     // consumer
-		true,   // auto-ack
-		false,  // exclusive
-		false,  // no-local
-		false,  // no-wait
-		nil,    // args
+		"testQueue", // queue
+		"",          // consumer
+		true,        // auto-ack
+		false,       // exclusive
+		false,       // no-local
+		false,       // no-wait
+		nil,         // args
 	)
 	failOnError(err, "Failed to register a consumer")
 
@@ -46,10 +36,11 @@ func main() {
 
 	go func() {
 		for d := range msgs {
+			// fmt.Println("RECEIVED")
 			log.Printf("Received a message: %s", d.Body)
 		}
 	}()
 
 	log.Printf(" [*] Waiting for messages. To exit press CTRL+C")
-	<-forever
+	<-forever // Keeps the main function running up until what conditions
 }
