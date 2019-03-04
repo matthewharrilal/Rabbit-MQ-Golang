604e46879323698ae24ee5d359a92fa40e2a9bd8
Matthew Harrilal <matthewharrilal@gmail.com>
2019-03-03 21:21:21 -0800

    Havent tested acknowledgement with legit long running task

--- recieve.go
+++ recieve.go
@@ -26,7 +26,7 @@ func main() {
 	msgs, err := ch.Consume(
 		"testQueue", // queue
 		"",          // consumer
-		true,        // auto-ack
+		false,       // auto-ack
 		false,       // exclusive
 		false,       // no-local
 		false,       // no-wait
